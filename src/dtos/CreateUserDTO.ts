3586747b6d5dda54a005d904656880f41a98136d
Mingmar Dorje Lama <mingmardorjetamang17@gmail.com>
2026-09-16 08:48:44 +0545

    feat: add RegisterUserDTO, LoginUserDTO, and AllUsersList interfaces

---
+++ src/dtos/CreateUserDTO.ts
@@ -0,0 +1,12 @@
+export interface RegisterUserDTO {
+    name: string;
+    email: string;
+    password: string;
+}
+export interface LoginUserDTO {
+    email: string,
+    password: string
+}
+export interface AllUsersList {
+    users: RegisterUserDTO[]
+}
